cafa435b7909f3807eaca70838afc5793db377b6
nattapat.s <nattapat.saenharn@gmail.com>
2021-09-11 09:11:14 +0700

    add owner function and update todo

--- Insurance.sol
+++ Insurance.sol
@@ -3,5 +3,21 @@
 pragma solidity ^0.8.0;
 
 contract Insurance {
+    // Owner of the system
+    address public owner;
+
+    // Constructor, can receive one or many variables here; only one allowed
+    constructor() {
+        // msg provides details about the message that's sent to the contract
+        // msg.sender is contract caller (address of contract creator)
+        owner = msg.sender;
+    }
     
+    // TODO Add hospital
+
+    // TODO Update hospital
+
+    // TODO Buy insurance
+
+    // TODO Function Claim
 }
